e22fc9a5da4973608a6b9dd0e46f4438f5106a69
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-30 12:38:49 -0400

    import Pages components to App.js, create restful routes and error page route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 // Dependencies
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Pages
+import Edit from "./Pages/Edit";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
 // Components
 import NavBar from "./Components/NavBar";
 
@@ -11,7 +19,12 @@ function App() {
         <NavBar />
         <main>
           <Routes>
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/logs" element={<Index />} />
+            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs/:id" element={<Show />} />
+            <Route path="/logs/:id/edit" element={<Edit />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </main>
       </Router>
