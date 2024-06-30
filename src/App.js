7ff54af304b5c077731e99cdba4fe52ee5f8ab60
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-30 12:34:10 -0400

    import NavBar component to App.js, setup initial Router and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 // Dependencies
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Components
+import NavBar from "./Components/NavBar";
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            
+          </Routes>
+        </main>
+      </Router>
     Captain's Log
     </div>
   )
