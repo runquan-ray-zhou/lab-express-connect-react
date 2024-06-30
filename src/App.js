91b48ab755ffd5b69cff3bf9135825a39b28db7a
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-06-30 12:46:00 -0400

    complete index.js pages component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Edit from "./Pages/Edit";
 // import Error from "./Pages/Error";
 import Home from "./Pages/Home";
-// import Index from "./Pages/Index";
+import Index from "./Pages/Index";
 // import New from "./Pages/New";
 // import Show from "./Pages/Show";
 
@@ -20,8 +20,8 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/logs" element={<Index />} />
-            <Route path="/logs/new" element={<New />} />
+            <Route path="/logs" element={<Index />} />
+            {/* <Route path="/logs/new" element={<New />} />
             <Route path="/logs/:id" element={<Show />} />
             <Route path="/logs/:id/edit" element={<Edit />} />
             <Route path="*" element={<Error />} /> */}
---
+++ src/Pages/Index.js
@@ -0,0 +1,10 @@
+import Logs from "../Components/Logs";
+
+export default function Index() {
+  return (
+    <div className="Index">
+      <h2>Index</h2>
+      <Logs />
+    </div>
+  );
+}
